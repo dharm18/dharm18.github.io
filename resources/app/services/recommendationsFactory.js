c3c16ed7cc55c1737bb12ad324b343dabe19bd8a
dharm18 <vishdharm.181910@gmail.com>
2017-07-27 01:18:15 +0530

    json

--- resources/app/services/recommendationsFactory.js
+++ resources/app/services/recommendationsFactory.js
@@ -1,7 +1,7 @@
 (function(){
     var recommendationsFactory = function($http) {
 
-        var url="http://site-vdharam.rhcloud.com/testimonial";
+        var url="testimonial.json";
 
         return {
         	retrieve: function(){
