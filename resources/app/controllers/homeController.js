b3695b8b8168e80fc4b815d888fd5db8324fb835
dharm18 <vishdharm.181910@gmail.com>
2017-10-14 21:54:26 +0530

    content updated

--- resources/app/controllers/homeController.js
+++ resources/app/controllers/homeController.js
@@ -5,8 +5,10 @@
 
     var homeController = function($scope) {
         $(".element").typed({
-            strings: ["Java", "J2EE","Spring","Hibernate","Grails","ZK","JSP","Servlets","Spring MVC","javascript","jQuery","AngularJS",
-                "Oracle","MySQL"],
+            /*strings: ["Java", "J2EE","Spring","Hibernate","Grails","ZK","JSP","Servlets","Spring MVC","javascript","jQuery","AngularJS",
+                "Oracle","MySQL"],*/
+                strings: ["Java", "J2EE","Grails","ZK","javascript","jQuery","angularJS","JSP","Servlets","Spring MVC",
+                    "Oracle","MySQL","Spring","Hibernate","Want to learn Hadoop/Big data","Interested in data-science, AI"],
             typeSpeed: 30
         });
 
